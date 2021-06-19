650c6076c6644ddb469ef7dddb05b66584fa93be
akarshgangwar28 <akarshprog28@gmail.com>
2021-06-19 11:28:11 +0530

    .

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+const express = require("express");
 const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
 mongoose.connect('mongodb://localhost:27017/solo-learn', {
     useNewUrlParser: true,
@@ -8,6 +14,26 @@ mongoose.connect('mongodb://localhost:27017/solo-learn', {
 })
 
 const db = mongoose.connection;
+
+app.get("/courseUpload",function(req ,res){
+    res.render("course")
+
+})
+
+app.get("/courseView",function(req,res){
+    res.render("courseView")
+
+})
+
+app.get("/",function(req,res){
+    res.send("<p>Its testing purpose</p>")
+
+})
+
+app.listen(3005, function() {
+    console.log("Server started on port 3005");
+  });
+
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
